fbe412381cc165f9fbfcfc277ac6ddcf9c54e7ff
Kosi Mensah <kmensah@usf.edu>
2024-11-03 08:48:42 -0500

    Handle errors with .catch() and display error message.

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
             if (!response.ok) {
                 throw new Error('Network response was not up to par');
             }
-            // Convert response to JSON
             return response.json();
         })
         .then(data => {
@@ -19,7 +18,27 @@ document.addEventListener('DOMContentLoaded', () => {
             displayProducts(data);
         }).catch(error => {
             // Task 4: Handle Errors 
-            errorMessage.textContent = 'Failed to load products. Please try again at a later time.';
+            errorMessage.textContent = 'There was a problem loading the products at this time. Please try again in a few minutes.';
             console.error('Fetch error:', error);
         });
 });
+// Task 3: Display Product Details Dynamically
+function displayProducts(products) {
+    // Get the product container element
+    const productContainer = document.getElementById('productContainer');
+
+    products.forEach(product => {
+        const productElement = document.createElement('div');
+
+        // Add HTML content for the product
+        productElement.innerHTML = `
+            <img src="${product.fields.image[0].url}" alt="${product.fields.name}">
+            <h2>${product.fields.name}</h2>
+            <p>Price: ${product.fields.price/100}</p>
+            <p>Company: $${product.fields.company}</p>
+        `;
+
+        // Add the product element to the container
+        productContainer.appendChild(productElement);
+    });
+}
